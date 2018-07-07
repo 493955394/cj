3330eb1a7d1e6e7b427cac467cfbdee3906c774d
493955394@qq.com <493955394@qq.com>
2018-07-07 12:31:10 +0800

    first add

---
+++ Application/Common/Conf/config.php
@@ -0,0 +1,7 @@
+<?php
+return array(
+	//'配置项'=>'配置值'
+    'DEFAULT_MODULE'	=> 'home',
+    'MODULE_ALLOW_LIST'	=> array('Home','Admin','Yan','Sheng'),
+    'APP_SUB_DOMAIN_DEPLOY'   =>    TRUE,
+);
